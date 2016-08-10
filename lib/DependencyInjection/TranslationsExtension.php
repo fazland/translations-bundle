06b4ea7ca01c1b3d7437d40b337fec36e38dea63
Giovanni Albero <giovanni.albero@fazland.com>
2016-08-10 22:22:08 +0200

    Created TranslationsExtension

---
+++ lib/DependencyInjection/TranslationsExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fazland\TranslationsBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class TranslationsExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
