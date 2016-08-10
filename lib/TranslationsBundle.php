ba59322bab79e27244d70be87ae951b18d04d1ca
Giovanni Albero <giovanni.albero@fazland.com>
2016-08-10 21:53:52 +0200

    Created the bundle's class

---
+++ lib/TranslationsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Fazland\TranslationsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Alessandro Chitolina <alekitto@gmail.com>
+ * @author Giovanni Albero <giovannialbero.solinf@gmail.com>
+ */
+class TranslationsBundle extends Bundle
+{
+
+}
